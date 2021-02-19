99d68f5ce1625ef6b6c4fbbe3c3bdb37e6af6c8d
castorga <36180482+castorga@users.noreply.github.com>
2021-02-19 11:16:27 -0800

    Practica1
    Practica 1 problema

---
+++ ddi2021-1/Assets/Practica1/Problema.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problema : MonoBehaviour
+{
+    void Start()
+    {
+        int[] nums = new int[]{8, 1, 2, 2, 3};
+        Debug.Log("[" + string.Join(",", new List<int>(smallerNumberCounter(nums)).ConvertAll(i => i.ToString()).ToArray()) + "]");
+    }
+
+    private int[] smallerNumberCounter(int[] array) {
+        //Complejidad O(n2) debido al uso de bucles doblemende anidados
+        int[] numbers = new int[array.Length];
+        for(int i = 0; i < array.Length; i++) {
+            int counter = 0;
+            for(int j = 0; j < array.Length; j++) {
+                if(i == j) {
+                    continue;
+                }
+                if(array[i] > array[j]) {
+                    counter++;
+                }
+            }
+            numbers[i] = counter;
+        }
+        return numbers;
+    }
+}
